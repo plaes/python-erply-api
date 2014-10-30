d9106472c0be3fb06ae0f68cd2e64f0975ecf7e7
Priit Laes <plaes@plaes.org>
2014-10-30 14:55:08 +0200

    Implement basic login support

--- erply_api.py
+++ erply_api.py
@@ -8,6 +8,7 @@
     :copyright: (c) 2014 by Priit Laes
     :license: BSD, see LICENSE for details.
 """
+import requests
 
 
 class ErplyAuth(object):
@@ -17,8 +18,14 @@ class ErplyAuth(object):
         self.username = username
         self.password = password
 
+    @property
+    def data(self):
+        return {'username': self.username,
+                'password': self.password}
 
 class Erply(object):
+
+    ERPLY_GET = ('getProducts', 'verifyUser')
 
     def __init__(self, auth):
         self.auth = auth
@@ -26,21 +33,55 @@ class Erply(object):
 
     @property
     def _payload(self):
-        return {'clientcode': {}.format(self.auth.code), 'version': '1.0'}
+        return {'clientCode': self.auth.code}
 
     @property
     def session(self):
-        raise NotImplementedError
+        def authenticate():
+            response = self.verifyUser(**self.auth.data)
+            # TODO: handle response
+            raise NotImplementedError
+        return self._key if self._key else authenticate()
 
     @property
     def payload(self):
-        p = self._payload
-        p.update({'sessionKey': self.session})
-        return p
+        return dict(sessionKey=self.session, **self._payload)
 
     @property
     def api_url(self):
         return 'https://{}.erply.com/api/'.format(self.auth.code)
+
+    @property
+    def headers(self):
+        return { 'Content-Type': 'application/x-www-form-urlencoded' }
+
+    def handle_get(self, request, _page=None, _response=None, *args, **kwargs):
+        data = dict(request=request)
+        data.update(self._payload if request == 'verifyUser' else self.payload)
+        data.update(kwargs)
+        if _page:
+            data['pageNo'] = _page + 1
+        r = requests.post(self.api_url, data=data, headers=self.headers)
+        if _response:
+            _response.update(r.json(), _page)
+        return ErplyResponse(self, r.json(), _page)
+
+    def __getattr__(self, attr):
+        if attr in self.ERPLY_GET:
+            def method(*args, **kwargs):
+                _page = kwargs.get('_page', None)
+                _response = kwargs.get('_response', None)
+                return self.handle_get(attr, _page, _response, *args, **kwargs)
+            self.__dict__[attr] = method
+            return method
+        raise AttributeError
+
+
+class ErplyResponse(object):
+
+    def __init__(self, erply, data, page):
+        print data
+        print page
 
 
 class ErplyPagedResult(object):
