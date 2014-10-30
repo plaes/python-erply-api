55e7dd9a74a9cec5352ee2bc23a4191caf7d1350
Priit Laes <plaes@plaes.org>
2014-10-30 21:52:03 +0200

    Implement CSV requests

--- erply_api.py
+++ erply_api.py
@@ -8,6 +8,9 @@
     :copyright: (c) 2014 by Priit Laes
     :license: BSD, see LICENSE for details.
 """
+from contextlib import closing
+from datetime import datetime
+import csv
 import requests
 
 
@@ -26,6 +29,7 @@ class ErplyAuth(object):
 class Erply(object):
 
     ERPLY_GET = ('getProducts', 'verifyUser')
+    ERPLY_CSV = ('getProductStockCSV',)
 
     def __init__(self, auth):
         self.auth = auth
@@ -59,6 +63,12 @@ class Erply(object):
     def headers(self):
         return { 'Content-Type': 'application/x-www-form-urlencoded' }
 
+    def handle_csv(self, request, *args, **kwargs):
+        data = dict(request=request, responseType='CSV')
+        data.update(self.payload)
+        data.update(**kwargs)
+        return ErplyCSVResponse(self, requests.post(self.api_url, data=data, headers=self.headers))
+
     def handle_get(self, request, _page=None, _per_page=None, _response=None, *args, **kwargs):
         data = dict(request=request)
         data.update(self.payload if request != 'verifyUser' else self._payload)
@@ -73,28 +83,35 @@ class Erply(object):
         return ErplyResponse(self, r, request, _page)
 
     def __getattr__(self, attr):
+        _attr = None
         if attr in self.ERPLY_GET:
             def method(*args, **kwargs):
                 _page = kwargs.get('_page', 0)
                 _response = kwargs.get('_response', None)
                 return self.handle_get(attr, _page, _response, *args, **kwargs)
-            self.__dict__[attr] = method
-            return method
+            _attr = method
+        elif attr in self.ERPLY_CSV:
+            def method(*args, **kwargs):
+                return self.handle_csv(attr.replace('CSV', ''), *args, **kwargs)
+            _attr = method
+        if _attr:
+            self.__dict__[attr] = _attr
+            return _attr
         raise AttributeError
 
 
 class ErplyResponse(object):
 
-    def __init__(self, erply, r, request, page=0):
+    def __init__(self, erply, response, request, page=0):
         self.request = request
         self.erply = erply
         self.error = None
 
-        if r.status_code != requests.codes.ok:
-            print ('Request failed with error code {}'.format(r.status_code))
+        if response.status_code != requests.codes.ok:
+            print ('Request failed with error code {}'.format(response.status_code))
             raise ValueError
 
-        data = r.json()
+        data = response.json()
         status = data.get('status', {})
 
         if not status:
@@ -131,3 +148,33 @@ class ErplyResponse(object):
         if key not in self.records:
             self.fetch_records(key)
         return self.records[key]
+
+
+class ErplyCSVResponse(object):
+
+    def __init__(self, erply, response):
+        self.erply = erply
+
+        if response.status_code != requests.codes.ok:
+            print ('Request failed with error code {}'.format(response.status_code))
+            raise ValueError
+
+        data = response.json()
+        status = data.get('status', {})
+        if not status:
+            print ("Malformed response")
+            raise ValueError
+
+        self.error = status.get('errorCode')
+
+        self.url = data.get('records').pop().get('reportLink')
+        self.timestamp = datetime.fromtimestamp(status.get('requestUnixTime'))
+
+    @property
+    def records(self):
+        with closing(requests.get(self.url, stream=True)) as f:
+            if f.status_code != requests.codes.ok:
+                raise ValueError
+            reader = csv.reader(f.text.splitlines())
+            reader.next()
+            return reader
