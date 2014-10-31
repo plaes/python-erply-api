bb52cb1fd3c596a8c6142ed84858d82807e1c13d
Priit Laes <plaes@plaes.org>
2014-10-31 10:45:59 +0200

    Fix packaging

--- setup.py
+++ setup.py
@@ -15,5 +15,17 @@ setup(
     author='Priit Laes',
     author_email='plaes@plaes.org',
     long_description=__doc__,
-    packages=[],
+    install_requires=['requests'],
+    py_modules=['erply_api'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        'Topic :: Office/Business :: Financial :: Point-Of-Sale',
+        'Topic :: Software Development :: Libraries',
+    ]
 )
