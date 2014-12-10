acefafd45604b4444157b6678cbbc327d4ae1614
Priit Laes <plaes@plaes.org>
2014-12-10 09:59:02 +0200

    Move on to new development cycle

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup
 
 setup(
     name='ErplyAPI',
-    version='0-2014.12.10',
+    version='0-2014.12.10-dev',
     description='Python wrapper for Erply API',
     license='BSD',
     author='Priit Laes',
