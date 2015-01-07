7238a6a31500d41f5bb1bb5ee5460310d7a13a8e
Timo Tambet <ttambet@gmail.com>
2015-01-07 15:29:32 +0200

    Implement bulk request handling.

--- erply_api.py
+++ erply_api.py
@@ -90,7 +90,7 @@ class Erply(object):
 
     def handle_get(self, request, _page=None, _per_page=None, _response=None, *args, **kwargs):
         _is_bulk = kwargs.pop('_is_bulk', False)
-        data = kwargs
+        data = kwargs.copy()
         if _page:
             data['pageNo'] = _page + 1
         if _per_page:
@@ -98,7 +98,7 @@ class Erply(object):
         if _is_bulk:
             data.update(requestName=request)
             return data
-        data = update(request=request)
+        data.update(request=request)
         data.update(self.payload if request != 'verifyUser' else self._payload)
         r = requests.post(self.api_url, data=data, headers=self.headers)
         if _response:
@@ -107,13 +107,19 @@ class Erply(object):
 
     def handle_post(self, request, *args, **kwargs):
         _is_bulk = kwargs.pop('_is_bulk', False)
+        data = kwargs.copy()
         if _is_bulk:
-            raise NotImplementedError
-        data = dict(request=request)
+            data.update(requestName=request)
+            return data
+        data.update(request=request)
         data.update(self.payload)
-        data.update(**kwargs)
         r = requests.post(self.api_url, data=data, headers=self.headers)
         return ErplyResponse(self, r, request, *args, **kwargs)
+
+    def handle_bulk(self, _requests):
+        data = self.payload
+        data.update(requests=_requests)
+        return ErplyBulkResponse(self, requests.post(self.api_url, data=data))
 
     def __getattr__(self, attr):
         _attr = None
@@ -141,20 +147,22 @@ class Erply(object):
 
 
 class ErplyBulkRequest(object):
-    def __init__(self, erply):
+    def __init__(self, erply,  _json_dumps):
         self.calls = []
         self.erply = erply
+        self.json_dumper = _json_dumps
 
     def attach(self, attr, *args, **kwargs):
         if attr in self.erply.ERPLY_GET or attr in self.erply.ERPLY_POST:
             self.calls.append((getattr(self.erply, '{}_bulk'.format(attr)), args, kwargs))
 
-    def __call__(self):
-        # TODO: do the proper request and process the results
+    def __call__(self,):
+        _requests = []
         for n, request in enumerate(self.calls, start=1):
             _call, _args, _kwargs = request
             _kwargs.update(requestID=n)
-            print (_call(*_args, **_kwargs))
+            _requests.append(_call(*_args, **_kwargs))
+        return self.erply.handle_bulk(self.json_dumper(_requests))
 
 
 class ErplyResponse(object):
@@ -246,3 +254,34 @@ class ErplyCSVResponse(object):
             reader = csv.reader(f.text.splitlines())
             reader.next()
             return reader
+
+
+class ErplyBulkResponse(object):
+    def __init__(self, erply, response):
+        if response.status_code != requests.codes.ok:
+            print ('Request failed with error code {}'.format(response.status_code))
+            raise ValueError
+
+        self.data = response.json()
+        status = self.data.get('status', {})
+        if not status:
+            print ("Malformed response")
+            raise ValueError
+
+        self.error = status.get('errorCode')
+        self._requests = self.data.get('requests')
+
+
+    @property
+    def records(self):
+        if self._requests is None:
+            raise ValueError
+        for el in self._requests:
+            _status = el.get('status')
+            if _status.get('responseStatus') == 'error':
+                print ('Request failed: requestID: {} errorField: {}'.format(
+                        _status.get('requestID'),
+                        _status.get('errorField'),
+                       ))
+            else:
+                yield el.get('records')
