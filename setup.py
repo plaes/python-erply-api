c7dd12670bf5824b020af3ca0bf276d22af48e32
Priit Laes <plaes@plaes.org>
2015-01-07 15:40:25 +0200

    Release 0.2015.01.07 \o/
    Comes with basic Bulk requests support

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup
 
 setup(
     name='ErplyAPI',
-    version='0-2014.12.10-dev',
+    version='0.2015.01.07',
     description='Python wrapper for Erply API',
     license='BSD',
     author='Priit Laes',
