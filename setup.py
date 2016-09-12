398726b2c74562edccd7889a4dc69a9f3185a5e5
Priit Laes <plaes@plaes.org>
2016-09-12 07:34:57 +0300

    Release 0.2016.09.12 \o/

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup
 
 setup(
     name='ErplyAPI',
-    version='0.2015.01.16-dev',
+    version='0.2016.09.12',
     description='Python wrapper for Erply API',
     license='BSD',
     author='Priit Laes',
