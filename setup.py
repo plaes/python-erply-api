80cc5e80cd77ac873edc47849bf60ed588ff92af
Priit Laes <plaes@plaes.org>
2022-02-20 13:55:23 +0200

    Update setuptools metadata to up-to-date standards

--- setup.py
+++ setup.py
@@ -1,34 +1,8 @@
-#!/usr/bin/env python
-"""
-Erply-API
----------
+from setuptools import setup
 
-Python wrapper for Erply API
-"""
-from distutils.core import setup
-
+# Metadata is in setup.cfg. This file is mostly for Github's dependency graph.
 setup(
     name='ErplyAPI',
-    version='0.2016.09.12',
-    description='Python wrapper for Erply API',
-    license='BSD',
-    author='Priit Laes',
-    author_email='plaes@plaes.org',
-    long_description=__doc__,
-    install_requires=['requests'],
-    py_modules=['erply_api'],
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Internet :: WWW/HTTP :: Site Management',
-        'Topic :: Office/Business :: Financial :: Point-Of-Sale',
-        'Topic :: Software Development :: Libraries',
-    ]
+    install_requires=['requests[security]>=2.6.0'],
+    py_modules = ['erply_api']
 )
